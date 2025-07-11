b5f4403483c7ee85ba4e6634f60344465c7ce8cc
Fulldrivz <141808972+Fulldrivz@users.noreply.github.com>
2025-07-11 14:02:23 -0400

    who will pay the bill

---
+++ Random bill payment.py
@@ -0,0 +1,14 @@
+import random
+
+
+friends=["Guy", "Davy" , "Oshen","Steven"]
+#option number 1
+x=random.randint(0,3)
+print(friends[x])
+
+#option number 2
+
+choice=random.choice(friends)
+print(choice)
+print(len(friends))
+
